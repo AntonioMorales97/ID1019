f6d0e6811ac01e65878071a3a4cee9ccc6bb19e5
Antonio <antonio_97@live.se>
2019-03-07 14:24:20 +0100

    removed unnecessary matching

--- random_exercises/mirror_tree.ex
+++ random_exercises/mirror_tree.ex
@@ -1,7 +1,6 @@
 defmodule MirrorTree do
     def mirror(nil) do nil end
 
-    def mirror({:node, v, nil, nil}) do {:node, v, nil, nil} end # necessary if v exists
     def mirror({:node, v, left, right}) do
         {:node, v, mirror(right), mirror(left)}
     end
